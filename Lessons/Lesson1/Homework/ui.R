0137f0fe82b582c72e94728006aa7da9bef7a921
kadlecikova <38209844+kadlecikova@users.noreply.github.com>
2018-04-15 22:38:10 +0200

    Update ui.R

--- Lessons/Lesson1/Homework/ui.R
+++ Lessons/Lesson1/Homework/ui.R
@@ -1,21 +1,27 @@
 
 
 # Use a fluid Bootstrap layout
-fluidPage(    
+fluidPage(Input() element(s),
+          Output() element(s))
   
   # Give the page a title
   titlePanel("Lesson 1 - Homework"),
   
 
-  sidebarLayout(      
+  sidebarLayout(
+    # Sidebar with a slider input
     
-
-    sidebarPanel(
+    sidebarPanel( 
+      sliderInput("obs","Number of observations:",
+      min = 0,
+      max = 1000,
+      value = 500)
 
     ),
     
     
     mainPanel(
+       plotOutput("distPlot")
        
     )
     
