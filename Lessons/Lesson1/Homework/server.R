32d603e99bbffea8c4e4c630383164450c4fd3a1
kadlecikova <38209844+kadlecikova@users.noreply.github.com>
2018-04-22 22:52:42 +0200

    Update server.R

--- Lessons/Lesson1/Homework/server.R
+++ Lessons/Lesson1/Homework/server.R
@@ -1,6 +1,8 @@
 
 
 # Define a server for the Shiny app
-function(input, output) {
-output$chart<-renderPlot(hist(runif(100)))
-}
+shinyServer(function(input, output, session) {
+            output$uppercase <- renderText({
+            toupper(input$message)
+})
+})
